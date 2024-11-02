944e13644a61e3cc78952cef0499586f6e67bb4d
Rzasmataz <arzasnicki@icloud.com>
2024-11-02 17:22:40 -0500

    init for api

--- server/server.py
+++ server/server.py
@@ -9,6 +9,18 @@ CORS(app)
 def home():
     return jsonify({'message': 'Welcome to the Flask API!'})
 
+@app.route('/therable/entry', methods=['POST'])
+def saveEntry():
+    return jsonify({'message': ''})
+@app.route('/therable/entry', methods=['GET'])
+def getallEntries():
+    return jsonify({'message': 'fdshj'})
+@app.route('/therable/entry', methods=['PUT'])
+def editEntry():
+    return jsonify({'message': ''})
+@app.route('/therable/entry', methods=['DELETE'])
+def deleteEntry():
+    return jsonify({'message': ''})
 
 if __name__ == '__main__':
     app.run(debug=True, port = 8080)  
