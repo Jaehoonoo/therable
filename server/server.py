11716e8f8bf075c1facac40b839d7eeee7d09eb6
Rzasmataz <arzasnicki@icloud.com>
2024-11-02 17:37:05 -0500

    changes

--- server/server.py
+++ server/server.py
@@ -1,23 +1,44 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 
+import iris
+import json
+
+namespace="USER"
+port = "1972"
+hostname= "localhost"
+connection_string = f"{hostname}:{port}/{namespace}"
+username = "demo"
+password = "demo"
+
 # app instance
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/api/home', methods=['GET'])
-def home():
-    return jsonify({'message': 'Welcome to the Flask API!'})
-
 @app.route('/therable/entry', methods=['POST'])
 def saveEntry():
     return jsonify({'message': ''})
+
 @app.route('/therable/entry', methods=['GET'])
 def getallEntries():
-    return jsonify({'message': 'fdshj'})
+    tableName = request.json.get('tableName')
+    conn = iris.connect(connection_string, username, password)
+    cursor = conn.cursor()
+    try:
+        cursor.execute(f"Select * From {tableName}")
+        data = cursor.fetchall()
+    except Exception as inst:
+        return jsonify({"response": str(inst)})
+    cursor.close()
+    conn.commit()
+    conn.close()
+    print(data)
+    return jsonify({"response": data})
+
 @app.route('/therable/entry', methods=['PUT'])
 def editEntry():
     return jsonify({'message': ''})
+
 @app.route('/therable/entry', methods=['DELETE'])
 def deleteEntry():
     return jsonify({'message': ''})
