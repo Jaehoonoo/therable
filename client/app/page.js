e88043c6d2485f80e02077aacd8ce7429ec3fe36
tomqhuynh <tomxhuynh@gmail.com>
2024-11-02 23:39:17 -0500

    Merge branch 'main' of https://github.com/Jaehoonoo/thera

--- client/app/page.js
+++ client/app/page.js
@@ -1,18 +1,47 @@
-"use client";
-import styles from "./page.module.css";
-import { useEffect } from "react";
+// page.js
+import Image from 'next/image';
+import styles from '../styles/LandingPage.module.css';
+import './globals.css';
 
-export default function Home() {
-
-  useEffect(() => {
-    fetch("http://localhost:8080/api/home")
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data.message)
-      });
-  }, [])
-
+export default function LandingPage() {
   return (
-    <div>hi</div>
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <div className={styles.logoContainer}>
+          <Image 
+            src="/newlogo.png" 
+            alt="Therable Logo" 
+            width={40} 
+            height={40} 
+            className={styles.logoIcon}
+          />
+        </div>
+        <div className={styles.buttonGroup}> {/* New container for buttons */}
+          <button className={styles.loginBtn}>Login</button>
+          <button className={styles.loginBtn}>Sign Up</button> {/* New Sign Up button */}
+        </div>
+      </header>
+
+      <main className={styles.mainContent}>
+        <div className={styles.textContent}>
+          <h1>welcome to <span className={styles.brandName}>therable</span>.</h1>
+          <h1 className={styles.textContent}>
+            a curated, customizable health tool for <em>you</em>.
+          </h1>
+          <div className={styles.buttonContainer}>
+            <button className={styles.getStartedBtn}>Get Started!</button>
+          </div>
+        </div>
+        <div className={styles.imageContainer}>
+          <Image 
+            src="/mainerpick.png" 
+            alt="Therapy Session Illustration" 
+            width={800} 
+            height={400} 
+            className={styles.illustration} 
+          />
+        </div>
+      </main>
+    </div>
   );
 }
