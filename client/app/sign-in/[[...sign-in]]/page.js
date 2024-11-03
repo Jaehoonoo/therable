4730847983e5b6961127525293cf775a83b5aa60
GanyuLover <120891026+GanyuLover@users.noreply.github.com>
2024-11-03 06:24:56 -0600

    fixed bug

--- client/app/sign-in/[[...sign-in]]/page.js
+++ client/app/sign-in/[[...sign-in]]/page.js
@@ -5,7 +5,7 @@ export default function SignInPage() {
   return (
     <div className={styles.container}>
       <div className={styles.signInBox}>
-        <SignIn signUpUrl="sign-up" />
+        <SignIn signInUrl="sign-in" />
       </div>
       <div className={styles.imageWrapper}>
         <img src="writing.png" alt="Drawing of an individual writing in their journal" className={styles.image} />
--- client/app/sign-up/[[...sign-up]]/page.js
+++ client/app/sign-up/[[...sign-up]]/page.js
@@ -1,11 +1,11 @@
-import { SignIn } from "@clerk/nextjs";
+import { SignUp } from "@clerk/nextjs";
 import styles from './page.module.css';
 
-export default function SignInPage() {
+export default function SignUpPage() {
   return (
     <div className={styles.container}>
-      <div className={styles.signInBox}>
-        <SignIn signUpUrl="sign-up" />
+      <div className={styles.signUpBox}>
+        <SignUp signUpUrl="sign-up" />
       </div>
       <div className={styles.imageWrapper}>
         <img src="oldman.png" alt="Image of an older individual thinking of what to write in his journal" className={styles.image} />
