9acd85d196740bfd6a3bdecf5e789ce80720b063
GanyuLover <120891026+GanyuLover@users.noreply.github.com>
2024-11-03 06:10:46 -0600

    edited sign pages

--- client/app/sign-in/[[...sign-in]]/page.js
+++ client/app/sign-in/[[...sign-in]]/page.js
@@ -1,10 +1,15 @@
 import { SignIn } from "@clerk/nextjs";
-import styles from './page.module.css'
+import styles from './page.module.css';
 
 export default function SignInPage() {
   return (
     <div className={styles.container}>
-      <SignIn signUpUrl="sign-up"/>
+      <div className={styles.signInBox}>
+        <SignIn signUpUrl="sign-up" />
+      </div>
+      <div className={styles.imageWrapper}>
+        <img src="writing.png" alt="Description" className={styles.image} />
+      </div>
     </div>
   );
 }
--- client/app/sign-up/[[...sign-up]]/page.js
+++ client/app/sign-up/[[...sign-up]]/page.js
@@ -1,10 +1,15 @@
-import { SignUp } from "@clerk/nextjs";
-import styles from './page.module.css'
+import { SignIn } from "@clerk/nextjs";
+import styles from './page.module.css';
 
-export default function SignUpPage() {
+export default function SignInPage() {
   return (
     <div className={styles.container}>
-      <SignUp signInUrl="/sign-in" />
+      <div className={styles.signInBox}>
+        <SignIn signUpUrl="sign-up" />
+      </div>
+      <div className={styles.imageWrapper}>
+        <img src="oldman.png" alt="Description" className={styles.image} />
+      </div>
     </div>
   );
 }
