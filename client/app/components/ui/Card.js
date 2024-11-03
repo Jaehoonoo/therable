0a30bd110b797a0b3958d2ed6524505c501587ce
GanyuLover <120891026+GanyuLover@users.noreply.github.com>
2024-11-03 09:59:45 -0600

    color change for buttons

--- client/app/components/ui/Card.js
+++ client/app/components/ui/Card.js
@@ -1,6 +1,6 @@
 const Card = ({ children, className, ...props }) => {
     return (
-      <div className={`bg-white shadow-md rounded-lg p-4 ${className}`} {...props}>
+      <div className={`bg-#f0f7f4 shadow-md rounded-lg p-4 ${className}`} {...props}>
         {children}
       </div>
     );
